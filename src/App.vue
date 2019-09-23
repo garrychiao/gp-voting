0ca1798f4caa9b1366eeb1af3a750265690ad05f
garrychiao <g50905g@gmail.com>
2019-09-23 22:53:10 +0800

    basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,123 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row>
+      <!-- header img -->
+      <el-col :xs="24" :sm="12" class="header-img">
+
+      </el-col>
+      <!-- header title -->
+      <el-col :xs="24" :sm="12" class="header-title">
+        <el-row>
+          <el-col :xs="{span: 3, offset: 3}" :xl="{span: 1, offset: 5}">
+            <p class="title-Q Apercu-Bold">Q</p>
+          </el-col>
+          <el-col :xs="{span: 3, offset: 3}" :xl="{span: 12, offset: 3}">
+            <p class="title NotoSansCJKtc-Regular">臺灣<span class="Apercu-BoldItalic">9</span>大連鎖零售業中，<br>你最想先看到哪家<span class="NotoSansCJKtc-Medium">達成減塑</span>？</p>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :xs="{span: 3, offset: 3}" :xl="{span: 12, offset: 6}">
+            <p class="sub-title NotoSansCJKtc-Regular">｜ <span class="NotoSansCJKtc-Bold">點擊</span>品牌方塊，即可投票  ｜ </p>
+          </el-col>
+        </el-row>
+      </el-col>
+
+      <!-- block section -->
+      <el-col :xs="24" :sm="12">
+        <el-row class="block-section">
+          <el-col :xs="12" :sm="8" v-for="(item, index) in marts" :key="index">
+            <SelectBlock :targetMart="item"></SelectBlock>
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SelectBlock from './components/SelectBlock.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    SelectBlock
+  },
+  data () {
+    return {
+      marts: [
+        "愛買",
+        "家樂福",
+        "全聯福利中心",
+        "好市多",
+        "全家",
+        "大潤發",
+        "7-ELEVEN",
+        "頂好",
+        "美廉社",
+      ]
+    }
+  },
+  created() {
+    this.marts = this.shuffle(this.marts);
+    // console.log(this.marts)
+  },
+  methods: {
+    shuffle(array) {
+      let counter = array.length;
+      // While there are elements in the array
+      while (counter > 0) {
+          // Pick a random index
+          let index = Math.floor(Math.random() * counter);
+
+          // Decrease counter by 1
+          counter--;
+
+          // And swap the last element with it
+          let temp = array[counter];
+          array[counter] = array[index];
+          array[index] = temp;
+      }
+
+      return array;
+    }
+
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: NotoSansCJKtc-Regular,'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  letter-spacing: 2pt;
+}
+.header-img {
+  background-image: url('./assets/img/web-banner.png');
+  background-size: cover;
+  height: 100vh;
+}
+.header-title {
+  background-color: var(--main-color);
+  height: 25vh;
+  color: white;
+  padding: 40px;
+  .title-Q {
+    margin: 0;
+    font-size: 8rem;
+  }
+  .title {
+    font-size: 1.5rem;
+    margin-top: 40px;
+    letter-spacing: 3.15pt;
+  }
+  .sub-title {
+    font-size: 1.2rem;
+    letter-spacing: 3.15pt;
+    text-align:  center;
+  }
+}
+.block-section {
+  overflow: hidden;
 }
 </style>
---
+++ src/components/SelectBlock.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="select-block" v-bind:style="{backgroundImage: `url(${target.imgUrl})`}">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SelectBlock',
+  props: ['targetMart'],
+  data() {
+    return {
+      target: {
+        mart: '愛買',
+        engMart: "a.Mart",
+        imgUrl: require('@/assets/img/marts/a-mart.png')
+      },
+      marts: [
+        {
+          mart: '愛買',
+          engMart: "a.Mart",
+          imgUrl: require('@/assets/img/marts/a-mart.png')
+        },
+        {
+          mart: '家樂福',
+          engMart: "Carrefour",
+          imgUrl: require('@/assets/img/marts/carrefour.png')
+        },
+        {
+          mart: '全聯福利中心',
+          engMart: "PX Mart",
+          imgUrl: require('@/assets/img/marts/px-mart.png')
+        },
+        {
+          mart: '好市多',
+          engMart: "Costco",
+          imgUrl: require('@/assets/img/marts/costco.png')
+        },
+        {
+          mart: '全家',
+          engMart: "FamilyMart",
+          imgUrl: require('@/assets/img/marts/family.png')
+        },
+        {
+          mart: '大潤發',
+          engMart: "RT-Mart",
+          imgUrl: require('@/assets/img/marts/rt-mart.png')
+        },
+        {
+          mart: '7-ELEVEN',
+          engMart: "7-ELEVEN",
+          imgUrl: require('@/assets/img/marts/711.jpg')
+        },
+        {
+          mart: '頂好',
+          engMart: "Wellcome",
+          imgUrl: require('@/assets/img/marts/wellcome.jpg')
+        },
+        {
+          mart: '美廉社',
+          engMart: "Simple Mart",
+          imgUrl: require('@/assets/img/marts/a-mart.png')
+        },
+        
+      ]
+    }
+  },
+  created() {
+    this.bindTarget();
+  },
+  methods: {
+    bindTarget () {
+      let target = this.marts.find((element) => {
+        return element.mart === this.targetMart
+      });
+      if (target){
+        this.target = target;
+      }  
+    }
+  },
+  watch: {
+    targetMart: function () {
+      this.bindTarget();
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.select-block {
+  height: 25vh;
+  background-color: rgb(245, 245, 245);
+  background-size: 30% auto;
+  background-repeat: no-repeat;
+  background-position: center center;
+  margin: -1px;
+  border: 1px solid black;
+  cursor: pointer;
+}
+</style>
