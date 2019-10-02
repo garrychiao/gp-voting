0f3b5a0901657baa9cbf96d494245b9b9920bca4
garrychiao <g50905g@gmail.com>
2019-10-02 15:37:05 +0800

    gzip added

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,20 @@ module.exports = {
 
     filenameHashing: true,
 
-    
+    configureWebpack: config => {
+
+        if(process.env.NODE_ENV === 'production'){
+            return{
+                plugins: [
+                    new CompressionPlugin({
+                        test:/\.js$|\.html$|\.css$|\.otf/, //匹配文件名
+                        threshold: 10240,//对超过10k的数据压缩
+                        deleteOriginalAssets: false //不删除源文件
+                    })
+                ]
+            }
+        }
+
+    },
+
 };
