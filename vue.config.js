371b36029e704e504c0739470fa78efd6aec9fb6
garrychiao <g50905g@gmail.com>
2019-10-02 12:47:16 +0800

    gzip added

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const CompressionPlugin = require("compression-webpack-plugin")
+
 module.exports = {
     publicPath: './',
 
@@ -6,5 +8,21 @@ module.exports = {
     assetsDir: 'static',
 
     filenameHashing: true,
+
+    configureWebpack: config => {
+
+        if(process.env.NODE_ENV === 'production'){
+            return{
+                plugins: [
+                    new CompressionPlugin({
+                        test:/\.js$|\.html$|.\css/, //匹配文件名
+                        threshold: 10240,//对超过10k的数据压缩
+                        deleteOriginalAssets: false //不删除源文件
+                    })
+                ]
+            }
+        }
+
+    },
 
 };
